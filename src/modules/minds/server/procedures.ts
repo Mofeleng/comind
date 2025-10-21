a40bbe3ce78d9a60181aec3389c7a9c508caca78
Mofeleng <mofelengmokgope@gmail.com>
2025-10-21 19:03:49 +0200

    12: Minds data table

--- src/modules/minds/server/procedures.ts
+++ src/modules/minds/server/procedures.ts
@@ -3,7 +3,7 @@ import { minds } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import z from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 export const agentsRouter = createTRPCRouter({
     //TODO: Change getMany to use protected procedure
     getMany: protectedProcedure.query(async () => {
@@ -11,7 +11,10 @@ export const agentsRouter = createTRPCRouter({
         return data;
     }),
     getOne: protectedProcedure.input(z.object({id: z.string()})).query(async ({ input }) => {
-        const [existingMind] = await db.select().from(minds).where(eq(minds.id, input.id));
+        const [existingMind] = await db.select({
+            ...getTableColumns(minds),
+            sessionsCount: sql<number>`5`,
+        }).from(minds).where(eq(minds.id, input.id));
 
         return existingMind;
     }),
--- src/modules/minds/ui/views/minds-view.tsx
+++ src/modules/minds/ui/views/minds-view.tsx
@@ -4,13 +4,25 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
+
 
 export const MindsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.minds.getMany.queryOptions());
 
     return (
-        <div>{ JSON.stringify(data, null, 2) }</div>
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data} columns={columns}/>
+            { data.length === 0 && (
+                <EmptyState 
+                    title="Create your first mind"
+                    description="Create a mind to join your meetings, each mind will follow your instructions and interact with you or members of your sessions."
+                />
+            )}
+        </div>
     )
 }
 
