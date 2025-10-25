55d1fbb1412a5bd54ef52e7476c23c31e485713a
Mofeleng <mofelengmokgope@gmail.com>
2025-10-25 17:50:10 +0200

    28: Bugs

--- src/modules/sessions/ui/components/upcoming-state.tsx
+++ src/modules/sessions/ui/components/upcoming-state.tsx
@@ -2,16 +2,14 @@
 
 import { EmptyState } from "@/components/empty-state"
 import { Button } from "@/components/ui/button"
-import { BanIcon, VideoIcon } from "lucide-react";
+import { VideoIcon } from "lucide-react";
 import Link from "next/link";
 
 interface Props {
     sessionId: string;
-    onCancelSession: () => void;
-    isCancelling: boolean;
 }
 
-export const UpcomingState = ({ sessionId, onCancelSession, isCancelling }:Props) => {
+export const UpcomingState = ({ sessionId }:Props) => {
     return (
         <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
             <EmptyState
@@ -21,18 +19,8 @@ export const UpcomingState = ({ sessionId, onCancelSession, isCancelling }:Props
             />
             <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2">
                 <Button
-                    variant="secondary"
-                    className="w-full lg:auto"
-                    onClick={onCancelSession}
-                    disabled={isCancelling}
-                >
-                    <BanIcon />
-                    Cancel Session
-                </Button>
-                <Button
                     asChild 
                     className="w-full lg:auto"
-                    disabled={isCancelling}
                 >
                     <Link href={`/call/${sessionId}`}>
                         <VideoIcon />
